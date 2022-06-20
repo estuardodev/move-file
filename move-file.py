99f23917d54ced9103061e767c496edeb5191cc0
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-19 20:17:18 -0600

    Se creo el codigo para mover los archivos asignados a la carpeta asignada

--- move-file.py
+++ move-file.py
@@ -1,3 +1,5 @@
+from pickletools import optimize
+from statistics import quantiles
 from PIL import Image
 import os
 
@@ -5,3 +7,14 @@ folderAmover = "Users/Estuardo/Documents/Software/Move-File/ArchivosAMover/" # C
 folderImagenesMovidas = "Users/Estuardo/Documents/Software/Move-File/CarpetaImagenesMovidas/" # Carpeta donde se moveran los archivos png|jpg|jpeg|webp
 folderAudiosMovidos = "Users/Estuardo/Documents/Software/Move-File/CarpetaAudiosMovidos/" # Carpeta donde se moveran los archivos mp3
 folderTXTMovidos = "Users/Estuardo/Documents/Software/Move-File/CarpetaTXTMovidos/" #Carpeta donde se moveran los archvios TXT
+
+if __name__ == "__main__":
+    for filename in os.listdir(folderAmover):
+        name, extension = os.path.splitext(folderAmover + filename)
+
+        # Se moveran las extensiones .png|.jpg|.jpeg.webp a la carpeta asignada en folderImagenesMovidas
+        if extension in [".png", ".jpg", ".jpeg", ".webp"]:
+            picture = Image.open(folderAmover + filename)
+            picture.save(folderImagenesMovidas + "compressed_" + filename, optimize=True, quality=60)
+            os.remove(folderAmover + filename)
+            print(name + ": " + extension)
