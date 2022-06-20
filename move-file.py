054360aec7d0af94d042cc7c228c79c1a314fdca
Estuardo Ramirez <estuardito_9@hotmail.com>
2022-06-19 20:08:26 -0600

    Se crearon las variables de los directorios

---
+++ move-file.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import os
+
+folderAmover = "Users/Estuardo/Documents/Software/Move-File/ArchivosAMover/" # Carpeta fuente
+folderImagenesMovidas = "Users/Estuardo/Documents/Software/Move-File/CarpetaImagenesMovidas/" # Carpeta donde se moveran los archivos png|jpg|jpeg|webp
+folderAudiosMovidos = "Users/Estuardo/Documents/Software/Move-File/CarpetaAudiosMovidos/" # Carpeta donde se moveran los archivos mp3
+folderTXTMovidos = "Users/Estuardo/Documents/Software/Move-File/CarpetaTXTMovidos/" #Carpeta donde se moveran los archvios TXT
